a7acbcb669735b6882fb5fb4959dbadfd59050f3
Stephen Kimoi <moenisteve@gmail.com>
2023-10-03 09:39:19 +0300

    tailoring hardhat file for deployement

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
+// const PRIVATE_KEY = process.env.PRIVATE_KEY 
+// Remember to use the private key of a testing account
+// For better security practices, it's recommended to use npm i dotenv for storing secret variables
+
 module.exports = {
-  solidity: "0.8.18",
+  defaultNetwork: "swisstronik",
+  solidity: "0.8.19",
+  networks: {
+    swisstronik: {
+      url: "https://json-rpc.testnet.swisstronik.com/",
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
 };
