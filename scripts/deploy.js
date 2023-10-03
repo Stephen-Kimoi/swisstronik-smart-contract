f338ee6cc6ed80b560942127cf9e1eede4ebe0e5
Stephen Kimoi <moenisteve@gmail.com>
2023-10-03 10:13:26 +0300

    deploy script for deploying the smart contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,16 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const contract = await hre.ethers.deployContract("VotingSystem", ["MasugeVotersSystem"]);
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  await contract.waitForDeployment();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log(`Voting system contract deployed to ${contract.target}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
+
+// Voting system contract deployed to 0x0B73D490fF0368B4bDF6137F82afAC21E1FB5625
